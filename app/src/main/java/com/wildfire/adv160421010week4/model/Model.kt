0dfa8e59cd474c95fb1f32bc32ad9d49f12b666e
Pootish2 <160823747+Pootish2@users.noreply.github.com>
2024-03-19 20:23:53 +0700

    Changes 1
    All slides work done.

---
+++ app/src/main/java/com/wildfire/adv160421010week4/model/Model.kt
@@ -0,0 +1,9 @@
+package com.wildfire.adv160421010week4.model
+
+data class Student(
+    var id:String?,
+    var name:String?,
+    var dob:String?,
+    var phone:String?,
+    var photoUrl:String?
+)
